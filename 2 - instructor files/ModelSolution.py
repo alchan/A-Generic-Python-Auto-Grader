76f4a06c990cb5c202a8fbeab90f54f271207530
alchan <achan@uncfsu.edu>
2020-09-12 09:17:09 -0400

    Update ModelSolution.py

--- 2 - instructor files/ModelSolution.py
+++ 2 - instructor files/ModelSolution.py
@@ -13,7 +13,7 @@ def getRandomInput ():
     choice = random ()
     if choice < 0.75:
         return randrange (1, 1001) # valid input 1 to 1000
-    elif choice < 0.85:
+    elif choice < 0.90:
         return -randrange (1001) # invalid input -1000 to 0
     else:
         return random () * 1000 # invalid input 0.0 to 1000.0
